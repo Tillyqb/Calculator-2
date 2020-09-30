798beb72ffe170e5fb752dbccaa7f16ae089bbab
Matilda Q Basner <tillyqb@yahoo.com>
2020-09-30 23:24:26 +0000

    Added functionality for the power function and user inputs

--- calculator.py
+++ calculator.py
@@ -3,5 +3,22 @@
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
+def calculator(input_string):
+    """Reads a keyboard input string that is space delimited 
+       and prints the result of the called function.
+    """
 
-# Replace this with your code
+
+    hell = 'warm'
+    while not hell == 'frozen':
+        string_as_list = input_string.split(' ')
+        if string_as_list[0] == 'q':
+            hell = "frozen"
+        elif string_as_list[0] == 'pow':
+            print(power(float(string_as_list[1]), float(string_as_list[2])))
+            input_string = input('???')
+        
+    return 0
+
+input_string = input('???')
+calculator(input_string)
